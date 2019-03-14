5aa3394df5d2e89a3108abb0aa406feb98ea2e07
itggot-jakob-lundpersson <jakob.lundpersson@elev.ga.ntig.se>
2019-03-14 15:40:46 +0100

    Format, header,

---
+++ Site/JS/main.js
@@ -0,0 +1,8 @@
+function doStuff(event){
+    let menu = document.querySelector("menu")
+        menu.classList.toggle("activate")
+        hamborgare.classList.toggle("moveLeft")
+        shade.classList.toggle("activate")
+    }
+
+    
