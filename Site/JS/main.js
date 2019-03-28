952b73dec9c1429356e9d389bf22564e1e4c3cec
itggot-jakob-lundpersson <jakob.lundpersson@elev.ga.ntig.se>
2019-03-28 15:34:05 +0100

    Meny klar, sida 2 påbörjad

--- Site/JS/main.js
+++ Site/JS/main.js
@@ -1,9 +1,11 @@
 function menuToggle(event){
     let menu = document.querySelector("menu")
-    let klass = document.getElementsByClassName("klass")
-        menu.classList.toggle("activate_menu")
-        klass.classList.toggle("activate_menu")
-        klass.id.toggle("activate_menu")
+    let nav = document.querySelector("nav")
+    //let klass = document.getElementsByClassName("klass")
+    menu.classList.toggle("activate_menu")
+    nav.classList.toggle("activate_cross")
+    
+
     }
 
     
